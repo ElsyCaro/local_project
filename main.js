ceee688a3d00f62ef774f592025fb545b73ad424
Elsy Carolina Zambrano <108857263+ElsyCaro@users.noreply.github.com>
2024-02-28 15:57:13 -0600

    segunda modificacion en el codigo

--- main.js
+++ main.js
@@ -15,34 +15,39 @@
  //Obtener las palabras
  
  async function obtenerPalabra (token){
+ const palabra = [];
  try {
  const respuesta = await('/challenge/token', {
 
-   headers: {
-  Authorization: BearerTutoken
-    }
+ headers: {
+  Authorization: 'Bearer {token}',
+    },
  });
-
+ 
  if (respuesta.status === 404){
  return null; //no hay mas palabras
  }
- return respuesta 
+ return respuesta
 
  } catch (error) {
     console.error ('Errror al obtener la palabra' , error.message);
     throw error;
  }
 
- }
+}
+ while (true) {
+    const palabra = (respuesta.obtenerPalabra);
+ 
+    return palabra;
+ } 
 
  // ordenar alfabeticamente quitando la primera letra 
- async function crearClave (palabras){
+  function crearClave (palabras){
 
   const palabraOrdenadas = palabras.sort (( a, b) =>
   a.slice (1).localCompare(b.slice(1))
- 
- );
-
+  );
+  const palabraConsecutiva = palabraOrdenadas.join ('');
  //Juntar las palabras en una sola cadena 
 
 }
